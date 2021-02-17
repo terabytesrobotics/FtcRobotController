29e7af2ae74425afd5990c48042125ea3b544a4e
terabytes-4149 <terabytesrobotics@gmail.com>
2021-02-16 19:28:34 -0500

    add object id module

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyFIRSTJavaOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyFIRSTJavaOpMode.java
@@ -14,6 +14,8 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -68,6 +70,8 @@ public class MyFIRSTJavaOpMode extends LinearOpMode {
     // Class Members
     private OpenGLMatrix lastLocation = null;
     private VuforiaLocalizer vuforia = null;
+    private VuforiaTrackables targetsUltimateGoal = null;
+    private List<VuforiaTrackable> allTrackables = null;
 
     /**
      * This is the webcam we are to use. As with other hardware devices such as motors and
@@ -80,6 +84,12 @@ public class MyFIRSTJavaOpMode extends LinearOpMode {
     private float phoneYRotate    = 0;
     private float phoneZRotate    = 0;
 
+    /**
+     * {@link #tfod} is the variable we will use to store our instance of the TensorFlow Object
+     * Detection engine.
+     */
+    private TFObjectDetector tfod;
+
     @Override public void runOpMode() {
         //imu = hardwareMap.get(Gyroscope.class, "imu");
         flDcMotor = hardwareMap.get(DcMotor.class, "flMotor");
@@ -90,8 +100,140 @@ public class MyFIRSTJavaOpMode extends LinearOpMode {
         //   digitalTouch = hardwareMap.get(DigitalChannel.class, "digitalTouch");
         //   sensorColorRange = hardwareMap.get(DistanceSensor.class, "sensorColorRange");
 
-        webcamName = hardwareMap.get(WebcamName.class, "webcam");
+        initVuforia();
+        initTfod();
 
+        // WARNING:
+        // In this sample, we do not wait for PLAY to be pressed.  Target Tracking is started immediately when INIT is pressed.
+        // This sequence is used to enable the new remote DS Camera Preview feature to be used with this sample.
+        // CONSEQUENTLY do not put any driving commands in this loop.
+        // To restore the normal opmode structure, just un-comment the following line:
+
+        // Note: To use the remote camera preview:
+        // AFTER you hit Init on the Driver Station, use the "options menu" to select "Camera Stream"
+        // Tap the preview window to receive a fresh image.
+
+        try {
+            telemetry.addData("Status", "Initialized");
+            telemetry.addData(">", "Press Play to start");
+            telemetry.update();
+            waitForStart();
+            telemetry.clear();
+            telemetry.addData(">", "Started...Press 'A' to capture frame");
+            telemetry.update();
+
+            double XtgtPower;
+            double LYtgtPower;
+            double RYtgtPower;
+            double ServoPositionX;
+            double ServoPositionY;
+            double ServoMap;
+
+            targetsUltimateGoal.activate();
+            /**
+             * Activate TensorFlow Object Detection before we wait for the start command.
+             * Do it here so that the Camera Stream window will have the TensorFlow annotations visible.
+             **/
+            if (tfod != null) {
+                tfod.activate();
+
+                // The TensorFlow software will scale the input images from the camera to a lower resolution.
+                // This can result in lower detection accuracy at longer distances (> 55cm or 22").
+                // If your target is at distance greater than 50 cm (20") you can adjust the magnification value
+                // to artificially zoom in to the center of image.  For best results, the "aspectRatio" argument
+                // should be set to the value of the images used to create the TensorFlow Object Detection model
+                // (typically 16/9).
+                tfod.setZoom(2.5, 16.0/9.0);
+            }
+
+            while (opModeIsActive()) {
+                XtgtPower = -this.gamepad1.left_stick_x;
+                LYtgtPower = this.gamepad1.left_stick_y;
+                RYtgtPower = -this.gamepad1.left_stick_y;
+                ServoPositionX = this.gamepad1.right_stick_x;
+                ServoPositionY = this.gamepad1.right_stick_y;
+                ServoMap = (ServoPositionX - ServoPositionY);
+                flDcMotor.setPower(XtgtPower - LYtgtPower);
+                frDcMotor.setPower(XtgtPower - RYtgtPower);
+                blDcMotor.setPower(XtgtPower - LYtgtPower);
+                brDcMotor.setPower(XtgtPower - RYtgtPower);
+                telemetry.addData("X Target Power", XtgtPower);
+                telemetry.addData("flMotor Power", flDcMotor.getPower());
+                telemetry.addData("frMotor Power", frDcMotor.getPower());
+                telemetry.addData("blMotor Power", blDcMotor.getPower());
+                telemetry.addData("brMotor Power", blDcMotor.getPower());
+                //while(ServoMap<1) {
+                    servoTest.setPosition(ServoMap);
+                //}
+                telemetry.addData("ServoPos", servoTest.getPosition());
+                telemetry.addData("ServoMap", ServoMap);
+
+                if (tfod != null) {
+                    // getUpdatedRecognitions() will return null if no new information is available since
+                    // the last time that call was made.
+                    List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
+                    if (updatedRecognitions != null) {
+                        telemetry.addData("# Object Detected", updatedRecognitions.size());
+                        // step through the list of recognitions and display boundary info.
+                        int i = 0;
+                        for (Recognition recognition : updatedRecognitions) {
+                            telemetry.addData(String.format("label (%d)", i), recognition.getLabel());
+                            telemetry.addData(String.format("  left,top (%d)", i), "%.03f , %.03f",
+                                    recognition.getLeft(), recognition.getTop());
+                            telemetry.addData(String.format("  right,bottom (%d)", i), "%.03f , %.03f",
+                                    recognition.getRight(), recognition.getBottom());
+                        }
+                        telemetry.update();
+                    }
+                }
+
+                // check all the trackable targets to see which one (if any) is visible.
+                targetVisible = false;
+                for (VuforiaTrackable trackable : allTrackables) {
+                    if (((VuforiaTrackableDefaultListener) trackable.getListener()).isVisible()) {
+                        telemetry.addData("Visible Target", trackable.getName());
+                        targetVisible = true;
+
+                        // getUpdatedRobotLocation() will return null if no new information is available since
+                        // the last time that call was made, or if the trackable is not currently visible.
+                        OpenGLMatrix robotLocationTransform = ((VuforiaTrackableDefaultListener) trackable.getListener()).getUpdatedRobotLocation();
+                        if (robotLocationTransform != null) {
+                            lastLocation = robotLocationTransform;
+                        }
+                        break;
+                    }
+                }
+
+                // Provide feedback as to where the robot is located (if we know).
+                if (targetVisible) {
+                    // express position (translation) of robot in inches.
+                    VectorF translation = lastLocation.getTranslation();
+                    telemetry.addData("Pos (in)", "{X, Y, Z} = %.1f, %.1f, %.1f",
+                            translation.get(0) / mmPerInch, translation.get(1) / mmPerInch, translation.get(2) / mmPerInch);
+
+                    // express the rotation of the robot in degrees.
+                    Orientation rotation = Orientation.getOrientation(lastLocation, EXTRINSIC, XYZ, DEGREES);
+                    telemetry.addData("Rot (deg)", "{Roll, Pitch, Heading} = %.0f, %.0f, %.0f", rotation.firstAngle, rotation.secondAngle, rotation.thirdAngle);
+                } else {
+                    telemetry.addData("Visible Target", "none");
+                }
+                telemetry.addData("Status", "Running");
+                telemetry.update();
+            }
+        } finally {
+            // Disable Tracking when we are done;
+            targetsUltimateGoal.deactivate();
+
+            if (tfod != null) {
+                tfod.shutdown();
+            }
+        }
+    }
+
+    /**
+     * Initialize the Vuforia localization engine.
+     */
+    private void initVuforia() {
         /*
          * Configure Vuforia by creating a Parameter object, and passing it to the Vuforia engine.
          * We can pass Vuforia the handle to a camera preview resource (on the RC phone);
@@ -100,14 +242,10 @@ public class MyFIRSTJavaOpMode extends LinearOpMode {
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
         // VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+
+        webcamName = hardwareMap.get(WebcamName.class, "webcam");
         parameters.vuforiaLicenseKey = VUFORIA_KEY;
-
-        /*
-         * We also indicate which camera on the RC we wish to use.
-         */
         parameters.cameraName = webcamName;
-
-        // Make sure extended tracking is disabled for this example.
         parameters.useExtendedTracking = false;
 
         //  Instantiate the Vuforia engine
@@ -115,7 +253,7 @@ public class MyFIRSTJavaOpMode extends LinearOpMode {
 
         // Load the data sets for the trackable objects. These particular data
         // sets are stored in the 'assets' part of our application.
-        VuforiaTrackables targetsUltimateGoal = this.vuforia.loadTrackablesFromAsset("UltimateGoal");
+        targetsUltimateGoal = this.vuforia.loadTrackablesFromAsset("UltimateGoal");
         VuforiaTrackable blueTowerGoalTarget = targetsUltimateGoal.get(0);
         blueTowerGoalTarget.setName("Blue Tower Goal Target");
         VuforiaTrackable redTowerGoalTarget = targetsUltimateGoal.get(1);
@@ -128,7 +266,7 @@ public class MyFIRSTJavaOpMode extends LinearOpMode {
         frontWallTarget.setName("Front Wall Target");
 
         // For convenience, gather together all the trackable objects in one easily-iterable collection */
-        List<VuforiaTrackable> allTrackables = new ArrayList<VuforiaTrackable>();
+        allTrackables = new ArrayList<VuforiaTrackable>();
         allTrackables.addAll(targetsUltimateGoal);
 
         /*
@@ -209,94 +347,17 @@ public class MyFIRSTJavaOpMode extends LinearOpMode {
         for (VuforiaTrackable trackable : allTrackables) {
             ((VuforiaTrackableDefaultListener) trackable.getListener()).setPhoneInformation(robotFromCamera, parameters.cameraDirection);
         }
+    }
 
-        // WARNING:
-        // In this sample, we do not wait for PLAY to be pressed.  Target Tracking is started immediately when INIT is pressed.
-        // This sequence is used to enable the new remote DS Camera Preview feature to be used with this sample.
-        // CONSEQUENTLY do not put any driving commands in this loop.
-        // To restore the normal opmode structure, just un-comment the following line:
-
-        // Note: To use the remote camera preview:
-        // AFTER you hit Init on the Driver Station, use the "options menu" to select "Camera Stream"
-        // Tap the preview window to receive a fresh image.
-
-        try {
-            telemetry.addData("Status", "Initialized");
-            telemetry.addData(">", "Press Play to start");
-            telemetry.update();
-            waitForStart();
-            telemetry.clear();
-            telemetry.addData(">", "Started...Press 'A' to capture frame");
-            telemetry.update();
-
-            double XtgtPower;
-            double LYtgtPower;
-            double RYtgtPower;
-            double ServoPositionX;
-            double ServoPositionY;
-            double ServoMap;
-
-            targetsUltimateGoal.activate();
-
-            while (opModeIsActive()) {
-                XtgtPower = -this.gamepad1.left_stick_x;
-                LYtgtPower = this.gamepad1.left_stick_y;
-                RYtgtPower = -this.gamepad1.left_stick_y;
-                ServoPositionX = this.gamepad1.right_stick_x;
-                ServoPositionY = this.gamepad1.right_stick_y;
-                ServoMap = (ServoPositionX - ServoPositionY);
-                flDcMotor.setPower(XtgtPower - LYtgtPower);
-                frDcMotor.setPower(XtgtPower - RYtgtPower);
-                blDcMotor.setPower(XtgtPower - LYtgtPower);
-                brDcMotor.setPower(XtgtPower - RYtgtPower);
-                telemetry.addData("X Target Power", XtgtPower);
-                telemetry.addData("flMotor Power", flDcMotor.getPower());
-                telemetry.addData("frMotor Power", frDcMotor.getPower());
-                telemetry.addData("blMotor Power", blDcMotor.getPower());
-                telemetry.addData("brMotor Power", blDcMotor.getPower());
-                //while(ServoMap<1) {
-                    servoTest.setPosition(ServoMap);
-                //}
-                telemetry.addData("ServoPos", servoTest.getPosition());
-                telemetry.addData("ServoMap", ServoMap);
-
-                // check all the trackable targets to see which one (if any) is visible.
-                targetVisible = false;
-                for (VuforiaTrackable trackable : allTrackables) {
-                    if (((VuforiaTrackableDefaultListener) trackable.getListener()).isVisible()) {
-                        telemetry.addData("Visible Target", trackable.getName());
-                        targetVisible = true;
-
-                        // getUpdatedRobotLocation() will return null if no new information is available since
-                        // the last time that call was made, or if the trackable is not currently visible.
-                        OpenGLMatrix robotLocationTransform = ((VuforiaTrackableDefaultListener) trackable.getListener()).getUpdatedRobotLocation();
-                        if (robotLocationTransform != null) {
-                            lastLocation = robotLocationTransform;
-                        }
-                        break;
-                    }
-                }
-
-                // Provide feedback as to where the robot is located (if we know).
-                if (targetVisible) {
-                    // express position (translation) of robot in inches.
-                    VectorF translation = lastLocation.getTranslation();
-                    telemetry.addData("Pos (in)", "{X, Y, Z} = %.1f, %.1f, %.1f",
-                            translation.get(0) / mmPerInch, translation.get(1) / mmPerInch, translation.get(2) / mmPerInch);
-
-                    // express the rotation of the robot in degrees.
-                    Orientation rotation = Orientation.getOrientation(lastLocation, EXTRINSIC, XYZ, DEGREES);
-                    telemetry.addData("Rot (deg)", "{Roll, Pitch, Heading} = %.0f, %.0f, %.0f", rotation.firstAngle, rotation.secondAngle, rotation.thirdAngle);
-                } else {
-                    telemetry.addData("Visible Target", "none");
-                }
-                telemetry.addData("Status", "Running");
-                telemetry.update();
-            }
-        } finally {
-            // Disable Tracking when we are done;
-            targetsUltimateGoal.deactivate();
-        }
+    /**
+     * Initialize the TensorFlow Object Detection engine.
+     */
+    private void initTfod() {
+        int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
+                "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
+        tfodParameters.minResultConfidence = 0.8f;
+        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
+        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_FIRST_ELEMENT, LABEL_SECOND_ELEMENT);
     }
 }
-;
