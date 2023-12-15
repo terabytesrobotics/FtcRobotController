db1eb8900edd56d6adb792538796db6b4fc56269
Gordon1234567 <89874528+Gordon1234567@users.noreply.github.com>
2023-12-15 15:07:29 -0500

    make enum pointsOfInterest

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/pointsOfInterest.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.util;
+
+public enum pointsOfInterest {
+    blueOuterAudience(-36, 60),
+    blueOuterBack(12, 60),
+    blueInnerAudience(-36,36),
+    blueInnerBack(12, 36),
+    blueGateAudience(-36, 12),
+    blueGateBack(12,12),
+    redGateAudience(-36, -12),
+    redGateBack(12,-12),
+    redInnerAudience(-36, -36),
+    redInnerBack(12,-36),
+    redOuterAudience(-36, -60),
+    redOuterBack(12, -60),
+    redDiagonalAudience(-48, 24),
+    redDiagonalBack(24,-24),
+    blueDiagonalAudience(-48, -24),
+    blueDiagonalBack(24,24),
+    midGateAuidience(-36,0),
+    midGateBack(24,0);
+
+
+
+
+
+
+    public double X;
+    public double Y;
+
+    pointsOfInterest(double x, double y) {
+        X = x;
+        Y = y;
+    }
+
+}
