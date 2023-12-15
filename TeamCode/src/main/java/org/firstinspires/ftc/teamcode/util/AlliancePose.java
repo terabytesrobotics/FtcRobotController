f162ead4ff5bcd76d19d12a1c3d438ff5b084e96
terabytes <joel.lewis.alexander@gmail.com>
2023-12-14 20:57:22 -0500

    Correcting starting position headings

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/AlliancePose.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/AlliancePose.java
@@ -5,11 +5,11 @@ import com.acmerobotics.roadrunner.geometry.Pose2d;
 public enum AlliancePose {
 
     BACKSTAGE_START(
-            new Pose2d(12, 60, Math.toRadians(270)),
+            new Pose2d(12, 60, Math.toRadians(90)),
             new Pose2d(12, -60, Math.toRadians(270))),
     FRONTSTAGE_START(
             new Pose2d(-36, 60, Math.toRadians(90)),
-            new Pose2d(-36, -60, Math.toRadians(90)));
+            new Pose2d(-36, -60, Math.toRadians(270)));
 
     public Pose2d BluePose;
     public Pose2d RedPose;
