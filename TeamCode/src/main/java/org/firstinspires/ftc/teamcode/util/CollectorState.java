902f9f8a86c78878583f29c424dced3ce6f56204
terabytes <joel.lewis.alexander@gmail.com>
2024-01-19 16:53:39 -0500

    Adjust setpoints

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/CollectorState.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/CollectorState.java
@@ -3,9 +3,9 @@ package org.firstinspires.ftc.teamcode.util;
 public enum CollectorState {
     COLLECTION(-32, 18, 0.86f),
     DRIVING_SAFE(-20, 4, 0.8f),
-    SCORING(144, 0, 0.61f),
-    SCORING_MEDIUM(131, 12, 0.53f),
-    SCORING_HIGH(111, 18, 0.45f),
+    SCORING(139, 0, 0.59f),
+    SCORING_MEDIUM(131, 12, 0.529f),
+    SCORING_HIGH(125, 18, 0.429f),
     FAR_COLLECTION(-15, 18, 0.78f),
     LOW_SCORING(140, 0, 0.35f),
     SAFE_POSITION(0, 0, 0.5f),
