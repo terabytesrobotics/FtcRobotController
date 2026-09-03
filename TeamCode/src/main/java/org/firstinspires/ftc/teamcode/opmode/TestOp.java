ba93cdefae2c15eec2c485434c62fe5ca03cf587
ConnorD <314315503+cderu30@users.noreply.github.com>
2026-09-03 18:19:29 -0400

    pid debugging

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/TestOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/TestOp.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.opmode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.teamcode.Robot;
 
@@ -10,6 +11,8 @@ public class TestOp extends OpMode {
     Robot robot;
     private double kP = 0, kI = 0, kD = 0;
     private double kPDelta, kIDelta, kDDelta;
+    private double targetX = 0, targetY = 0;
+    private boolean moving = false;
 
     @Override
     public void init() {
@@ -20,13 +23,13 @@ public class TestOp extends OpMode {
     public void loop() {
         robot.update();
 
-        robot.moveTo(300, 300);
+        if (gamepad1.yWasPressed()) {
+            moving = !moving;
+        }
 
-        telemetry.addData("X coordinate", robot.getX());
-        telemetry.addData("Y coordinate", robot.getY());
-        telemetry.addData("Heading angle (deg)", robot.getHeading());
-
-
+        if (moving) {
+            robot.moveTo(targetX, targetY);
+        }
 
         if (gamepad1.dpadUpWasPressed()) {
             if (gamepad1.a) {
@@ -54,8 +57,30 @@ public class TestOp extends OpMode {
             }
         }
 
+        targetX += applyDeadband(-gamepad1.left_stick_y);
+        targetY += applyDeadband(gamepad1.left_stick_x);
+
+        targetX = Math.min(-300.0, targetX);
+        targetX = Math.max(300.0, targetX);
+
+        targetY = Math.min(-300.0, targetY);
+        targetY = Math.max(300.0, targetY);
+
         robot.driveTrainController.kP = kP;
         robot.driveTrainController.kI = kI;
         robot.driveTrainController.kD = kD;
+
+        telemetry.addData("X coordinate", robot.getX());
+        telemetry.addData("Y coordinate", robot.getY());
+        telemetry.addData("KP", kP);
+        telemetry.addData("KI", kI);
+        telemetry.addData("KD", kD);
+        telemetry.addData("Target X", targetX);
+        telemetry.addData("Target Y", targetY);
+//        telemetry.addData("Heading angle (deg)", robot.getHeading());
+    }
+
+    private double applyDeadband(double value) {
+        return Math.abs(value) < 0.05 ? 0.0 : Range.clip(value, -1.0, 1.0);
     }
 }
